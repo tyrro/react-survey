cfc064680c18a853b658f2adda7a2e11ac4b8ed3
tyrro <rajib.das.shuva@gmail.com>
2022-03-31 11:49:40 +0600

    [#18] chore: Restructure Alert component

--- components/Alert/index.jsx
+++ components/Alert/index.jsx
@@ -1,18 +1,19 @@
 import Image from 'next/image';
 
 export const alertTestIds = {
-  icon: 'alert-icon',
-  title: 'alert-title',
-  description: 'alert-description',
+  base: 'alert',
+  icon: 'alert__icon',
+  title: 'alert__title',
+  description: 'alert__description',
 };
 
 const Alert = ({ icon, title, description }) => {
   return (
-    <div className="w-full bg-notification backdrop-blur-2.5xl flex p-4 rounded-xl">
+    <div className="w-full bg-notification backdrop-blur-2.5xl flex p-4 rounded-xl" data-test-id={alertTestIds.base}>
       <span data-test-id={alertTestIds.icon}>
-        <Image src={icon} alt="alert" />
+        <Image src={icon} alt="alert" layout="fixed" width={30} height={30} />
       </span>
-      <div className="text-white ml-5">
+      <div className="text-white ml-4">
         <div className="text-base-small font-extrabold mb-2" data-test-id={alertTestIds.title}>
           {title}
         </div>
