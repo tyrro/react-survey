6284de99f20cc7f7c8c2621746d680f367610a69
tyrro <rajib.das.shuva@gmail.com>
2022-04-04 00:48:38 +0600

    [#10] feature: Implement user login backend

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,23 @@
+import { SWRConfig } from 'swr';
+
 import '@/styles/globals.scss';
 import '@/styles/fonts.scss';
 
+import fetchJson from 'lib/fetchJson';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <SWRConfig
+      value={{
+        fetcher: fetchJson,
+        onError: err => {
+          console.error(err);
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 }
 
 export default MyApp;
