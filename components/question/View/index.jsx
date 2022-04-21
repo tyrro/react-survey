ebb51c003404b7ad43e30f2fa41993be6aba1df4
tyrro <rajib.das.shuva@gmail.com>
2022-04-21 16:19:55 +0600

    [#29] chore: Satisfy vercel eslint

--- components/question/View/index.jsx
+++ components/question/View/index.jsx
@@ -2,8 +2,6 @@ import PropTypes from 'prop-types';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
-import { prepareQuestions } from '@/components/survey/View/prepareQuestions';
-
 import useUser from 'hooks/useUser';
 import useSurvey from 'hooks/useSurvey';
 
