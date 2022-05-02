614956de5195473e901a6e833156c2f13471efb4
tyrro <rajib.das.shuva@gmail.com>
2022-05-03 01:59:17 +0600

    [#38] chore: Add a new className props to Button component

--- components/Button/index.jsx
+++ components/Button/index.jsx
@@ -1,28 +1,29 @@
 import PropTypes from 'prop-types';
 
 export const SIZE_CLASSES = {
-  xs: 'w-[90px] h-10',
-  small: 'w-30 h-14',
-  medium: 'w-80 h-14',
-  large: 'w-[327px] h-14',
-  huge: 'w-[704px] h-14',
+  small: 'h-10',
+  large: 'h-14',
 };
 
-const Button = ({ size, dark = false, children, ...attributes }) => {
+const Button = ({ size, dark = false, className, children, ...attributes }) => {
   const sizeClass = SIZE_CLASSES[size];
   const backgroundClass = dark ? 'bg-dark text-white' : 'bg-white';
 
   return (
-    <button className={`text-base-large font-extrabold rounded-[10px] ${sizeClass} ${backgroundClass}`} {...attributes}>
+    <button
+      className={`text-base-large font-extrabold rounded-[10px] w-full ${className} ${sizeClass} ${backgroundClass}`}
+      {...attributes}
+    >
       {children}
     </button>
   );
 };
 
 Button.propTypes = {
-  size: PropTypes.oneOf(['xs', 'small', 'medium', 'large', 'huge']).isRequired,
+  size: PropTypes.oneOf(['small', 'large']).isRequired,
   dark: PropTypes.bool,
-  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node,
 };
 
 export default Button;
