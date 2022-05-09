6c46fb6aad0de63d8d6fef7ed71223b9207ca5ad
tyrro <rajib.das.shuva@gmail.com>
2022-05-09 16:34:56 +0600

    [#38] chore: Add react-overlays to close modal clicking outside of modal area

--- components/survey/Quit/index.jsx
+++ components/survey/Quit/index.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { useRouter } from 'next/router';
+import { useRootClose } from 'react-overlays';
 
 import Button from '@/components/Button';
 import Image from '@/components/Image';
@@ -11,12 +12,20 @@ export const surveyQuitTestIds = {
   quitSurveyIcon: 'survey-quit__close-icon',
   warningText: 'survey-quit__warning-text',
   quitConfirmationText: 'survey-quit__confirmation-text',
-  button: 'survey-quit__button',
+  quitConfirmationConfirmButton: 'survey-quit__confirm-button',
+  quitConfirmationCancelButton: 'survey-quit__cancel-button',
 };
 
 const SurveyQuit = () => {
   const router = useRouter();
+  const ref = useRef();
   const [quitConfirmationModalOpen, setQuitConfirmationModalOpen] = useState(false);
+
+  const handleRootClose = () => setQuitConfirmationModalOpen(false);
+
+  useRootClose(ref, handleRootClose, {
+    disabled: !quitConfirmationModalOpen,
+  });
 
   return (
     <>
@@ -30,6 +39,7 @@ const SurveyQuit = () => {
       {quitConfirmationModalOpen &&
         createPortal(
           <div
+            ref={ref}
             className="w-[313px] md:w-[390px] lg:w-[467px] absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 bg-sidebar/100 backdrop-blur-4xl rounded-[14px] p-6"
             data-test-id={surveyQuitTestIds.base}
           >
@@ -39,11 +49,21 @@ const SurveyQuit = () => {
             <div className="text-white/70 text-base-large mb-8" data-test-id={surveyQuitTestIds.quitConfirmationText}>
               Are you sure you want to quit the survey?
             </div>
-            <div className="w-[188px] flex ml-auto" data-test-id={surveyQuitTestIds.button}>
-              <Button size="small" dark className="mr-2" onClick={() => router.push('/')}>
+            <div className="w-[188px] flex ml-auto">
+              <Button
+                size="small"
+                dark
+                className="mr-2"
+                data-test-id={surveyQuitTestIds.quitConfirmationConfirmButton}
+                onClick={() => router.push('/')}
+              >
                 Yes
               </Button>
-              <Button size="small" onClick={() => setQuitConfirmationModalOpen(false)}>
+              <Button
+                size="small"
+                data-test-id={surveyQuitTestIds.quitConfirmationCancelButton}
+                onClick={() => setQuitConfirmationModalOpen(false)}
+              >
                 Cancel
               </Button>
             </div>
