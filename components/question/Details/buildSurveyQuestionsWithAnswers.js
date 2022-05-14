62013168d46ce71161384140ff10f55338b4537b
tyrro <rajib.das.shuva@gmail.com>
2022-05-15 00:43:17 +0600

    [#33] chore: Update buildSurveyQuestionsWithAnswers.js to include textarea responses

--- components/question/Details/buildSurveyQuestionsWithAnswers.js
+++ components/question/Details/buildSurveyQuestionsWithAnswers.js
@@ -1,8 +1,19 @@
-export const buildSurveyQuestionsWithAnswers = (questionId, answerId) => ({
-  id: questionId,
-  answers: [
-    {
+export const buildSurveyQuestionsWithAnswers = (questionId, answerId, textInputValue) => {
+  let answerObject;
+
+  if (textInputValue) {
+    answerObject = {
       id: answerId,
-    },
-  ],
-});
+      answer: textInputValue,
+    };
+  } else {
+    answerObject = {
+      id: answerId,
+    };
+  }
+
+  return {
+    id: questionId,
+    answers: [answerObject],
+  };
+};
