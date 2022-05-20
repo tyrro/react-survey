a126e099b5bc38369680909664575bb0c7ed928e
tyrro <rajib.das.shuva@gmail.com>
2022-05-20 13:48:41 +0600

    [#20] chore: Add SurveyDetails page UI tests

---
+++ cypress/integration/SurveyDetails/index.spec.js
@@ -0,0 +1,34 @@
+const surveyDetailsPageTestIds = {
+  coverImageUrl: 'survey-details__cover_image_url',
+  title: 'survey-details__cover_image_url',
+  introText: 'survey-details__intro-text',
+  startSurveyButton: 'survey-details__start-survey',
+};
+
+describe('Survey details page', () => {
+  const surveyId = 'd5de6a8f8f5f1cfe51bc';
+
+  beforeEach(() => {
+    cy.intercept('GET', '**/api/v1/surveys/*', { fixture: 'Survey/Details/valid.json' });
+
+    cy.login();
+
+    cy.visit(`/surveys/${surveyId}`);
+  });
+
+  it('renders the survey details', () => {
+    cy.findByTestId(surveyDetailsPageTestIds.coverImageUrl).should('be.visible');
+    cy.findByTestId(surveyDetailsPageTestIds.introText).should('be.visible');
+    cy.findByTestId(surveyDetailsPageTestIds.title).should('be.visible');
+  });
+
+  it('renders a button to start the survey', () => {
+    cy.findByTestId(surveyDetailsPageTestIds.startSurveyButton).should('be.visible');
+
+    cy.findByTestId(surveyDetailsPageTestIds.startSurveyButton).click();
+
+    cy.location().should(location => {
+      expect(location.pathname).to.eq(`/surveys/${surveyId}/questions`);
+    });
+  });
+});
