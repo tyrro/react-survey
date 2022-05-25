52f3c34d003c0baf9766baf876a495d31cdeef4d
tyrro <rajib.das.shuva@gmail.com>
2022-05-25 18:15:25 +0600

    [#20] chore: Introduce start-server-and-test

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,8 +1,5 @@
-const sortOrderSmacss = require('stylelint-config-property-sort-order-smacss/generate');
-
 module.exports = {
-  extends: ['stylelint-config-sass-guidelines'],
-  plugins: ['stylelint-scss', 'stylelint-order'],
+  extends: ['@nimblehq/stylelint-config-nimble'],
   rules: {
     'scss/at-rule-no-unknown': [
       true,
@@ -10,8 +7,5 @@ module.exports = {
         ignoreAtRules: ['tailwind', 'apply', 'layer', 'variants', 'responsive', 'screen'],
       },
     ],
-    'order/properties-alphabetical-order': null,
-    'order/properties-order': [sortOrderSmacss({ emptyLineBefore: 'always' })],
-    'max-nesting-depth': 3,
   },
 };
