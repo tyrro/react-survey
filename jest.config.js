50c11143c336e8f759676923c3609a36c5ecffe4
tyrro <rajib.das.shuva@gmail.com>
2022-06-08 23:44:41 +0600

    chore: Use jest shard option for faster execution

--- jest.config.js
+++ jest.config.js
@@ -16,6 +16,7 @@ const customJestConfig = {
   },
   testEnvironment: 'jest-environment-jsdom',
   testPathIgnorePatterns: ['<rootDir>/cypress/'],
+  coverageDirectory: '<rootDir>/coverage/jest',
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
